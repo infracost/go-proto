0a62e5cf66bca050a814130702326b39c0000059
Liam Cervante <liam.cervante@infracost.io>
2026-02-25 15:35:15 +0100

    feat: add support for checking canonical providers (#3)

---
+++ pkg/providers/provider.go
@@ -0,0 +1,29 @@
+package providers
+
+import "github.com/infracost/proto/gen/go/infracost/provider"
+
+func ToProto(raw string) provider.Provider {
+	switch raw {
+	case "aws":
+		return provider.Provider_PROVIDER_AWS
+	case "google":
+		return provider.Provider_PROVIDER_GOOGLE
+	case "azurerm":
+		return provider.Provider_PROVIDER_AZURERM
+	default:
+		return provider.Provider_PROVIDER_UNSPECIFIED
+	}
+}
+
+func FromProto(raw provider.Provider) string {
+	switch raw {
+	case provider.Provider_PROVIDER_AWS:
+		return "aws"
+	case provider.Provider_PROVIDER_GOOGLE:
+		return "google"
+	case provider.Provider_PROVIDER_AZURERM:
+		return "azurerm"
+	default:
+		return ""
+	}
+}

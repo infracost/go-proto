8e7c8f8d02253e8bc7cd6361de299998ae9e60ec
Liam Galvin <liam@infracost.io>
2026-03-24 09:28:31 +0000

    chore: Remove elastic inference (sunset by aws) (#8)

--- pkg/tree/aws/ec2/launch_template.go
+++ pkg/tree/aws/ec2/launch_template.go
@@ -7,19 +7,18 @@ import (
 
 type LaunchTemplate struct {
 	resource.Resource                `tree:"-"`
-	Name                             value.String                                 `tree:"name"`
-	InstanceType                     value.String                                 `tree:"instance_type"`
-	AMI                              value.String                                 `tree:"ami"`
-	EBSOptimized                     value.Bool                                   `tree:"ebs_optimised"`
-	MonitoringEnabled                value.Bool                                   `tree:"monitoring_enabled"`
-	CPUCredits                       value.Value[CPUCreditOption]                 `tree:"cpu_credits"`
-	Tenancy                          value.Value[Tenancy]                         `tree:"tenancy"`
-	MarketType                       value.Value[PurchaseOption]                  `tree:"market_type"`
-	BlockDeviceMappings              []BlockDeviceMapping                         `tree:"block_device_mappings"`
-	NetworkInterfaces                []NetworkInterface                           `tree:"network_interfaces"`
-	OnDemandPercentageAboveBaseCount value.Int                                    `tree:"on_demand_percentage_above_base_count"`
-	ElasticInferenceAcceleratorType  value.Value[ElasticInferenceAcceleratorType] `tree:"elastic_inference_accelerator_type"`
-	MetadataOptions                  InstanceMetadataOptions                      `tree:"metadata_options"`
+	Name                             value.String                 `tree:"name"`
+	InstanceType                     value.String                 `tree:"instance_type"`
+	AMI                              value.String                 `tree:"ami"`
+	EBSOptimized                     value.Bool                   `tree:"ebs_optimised"`
+	MonitoringEnabled                value.Bool                   `tree:"monitoring_enabled"`
+	CPUCredits                       value.Value[CPUCreditOption] `tree:"cpu_credits"`
+	Tenancy                          value.Value[Tenancy]         `tree:"tenancy"`
+	MarketType                       value.Value[PurchaseOption]  `tree:"market_type"`
+	BlockDeviceMappings              []BlockDeviceMapping         `tree:"block_device_mappings"`
+	NetworkInterfaces                []NetworkInterface           `tree:"network_interfaces"`
+	OnDemandPercentageAboveBaseCount value.Int                    `tree:"on_demand_percentage_above_base_count"`
+	MetadataOptions                  InstanceMetadataOptions      `tree:"metadata_options"`
 }
 
 type NetworkInterface struct {

ba0f77b5c74997dc8ba7b51827e83a7b84a37d07
Liam Galvin <liam@infracost.io>
2026-03-24 10:09:22 +0000

    feat: Add launch template tag spec support

--- pkg/tree/aws/ec2/launch_template.go
+++ pkg/tree/aws/ec2/launch_template.go
@@ -6,19 +6,22 @@ import (
 )
 
 type LaunchTemplate struct {
-	resource.Resource                `tree:"-"`
-	Name                             value.String                 `tree:"name"`
-	InstanceType                     value.String                 `tree:"instance_type"`
-	AMI                              value.String                 `tree:"ami"`
-	EBSOptimized                     value.Bool                   `tree:"ebs_optimised"`
-	MonitoringEnabled                value.Bool                   `tree:"monitoring_enabled"`
-	CPUCredits                       value.Value[CPUCreditOption] `tree:"cpu_credits"`
-	Tenancy                          value.Value[Tenancy]         `tree:"tenancy"`
-	MarketType                       value.Value[PurchaseOption]  `tree:"market_type"`
-	BlockDeviceMappings              []BlockDeviceMapping         `tree:"block_device_mappings"`
-	NetworkInterfaces                []NetworkInterface           `tree:"network_interfaces"`
-	OnDemandPercentageAboveBaseCount value.Int                    `tree:"on_demand_percentage_above_base_count"`
-	MetadataOptions                  InstanceMetadataOptions      `tree:"metadata_options"`
+	resource.Resource                 `tree:"-"`
+	Name                              value.String                 `tree:"name"`
+	InstanceType                      value.String                 `tree:"instance_type"`
+	AMI                               value.String                 `tree:"ami"`
+	EBSOptimized                      value.Bool                   `tree:"ebs_optimised"`
+	MonitoringEnabled                 value.Bool                   `tree:"monitoring_enabled"`
+	CPUCredits                        value.Value[CPUCreditOption] `tree:"cpu_credits"`
+	Tenancy                           value.Value[Tenancy]         `tree:"tenancy"`
+	MarketType                        value.Value[PurchaseOption]  `tree:"market_type"`
+	BlockDeviceMappings               []BlockDeviceMapping         `tree:"block_device_mappings"`
+	NetworkInterfaces                 []NetworkInterface           `tree:"network_interfaces"`
+	OnDemandPercentageAboveBaseCount  value.Int                    `tree:"on_demand_percentage_above_base_count"`
+	MetadataOptions                   InstanceMetadataOptions      `tree:"metadata_options"`
+	InstanceTagSpecifications         resource.Tags                `tree:"instance_tag_specifications"`
+	VolumeTagSpecifications           resource.Tags                `tree:"volume_tag_specifications"`
+	NetworkInterfaceTagSpecifications resource.Tags                `tree:"network_interface_tag_specifications"`
 }
 
 type NetworkInterface struct {

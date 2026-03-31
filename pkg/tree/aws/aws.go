e4c4119a56a3dc89eb582c2106815cb0fb510ca3
Owen Rumney <owen@infracost.io>
2026-03-31 16:39:16 +0100

    feat: Add cloudformation types

--- pkg/tree/aws/aws.go
+++ pkg/tree/aws/aws.go
@@ -6,6 +6,7 @@ import (
 	"github.com/infracost/go-proto/pkg/tree/aws/apigateway"
 	"github.com/infracost/go-proto/pkg/tree/aws/apigatewayv2"
 	"github.com/infracost/go-proto/pkg/tree/aws/appautoscaling"
+	"github.com/infracost/go-proto/pkg/tree/aws/cloudformation"
 	"github.com/infracost/go-proto/pkg/tree/aws/ec2"
 )
 
@@ -16,6 +17,7 @@ type AWS struct {
 	APIGateway              apigateway.APIGateway          `tree:"apigateway"`
 	APIGatewayV2            apigatewayv2.APIGatewayV2      `tree:"apigatewayv2"`
 	AppAutoScaling          appautoscaling.AppAutoScaling  `tree:"appautoscaling"`
+	CloudFormation          cloudformation.CloudFormation  `tree:"cloudformation"`
 }
 
 func (aws *AWS) PostProcess() {

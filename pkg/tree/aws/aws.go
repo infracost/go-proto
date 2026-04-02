1ae9b58ace12492ddbf62024bd125a7a46d93b11
Owen Rumney <owen@infracost.io>
2026-04-02 13:58:30 +0100

    feat: add elasticbeanstalk types

--- pkg/tree/aws/aws.go
+++ pkg/tree/aws/aws.go
@@ -7,15 +7,17 @@ import (
 	"github.com/infracost/go-proto/pkg/tree/aws/apigatewayv2"
 	"github.com/infracost/go-proto/pkg/tree/aws/appautoscaling"
 	"github.com/infracost/go-proto/pkg/tree/aws/ec2"
+	"github.com/infracost/go-proto/pkg/tree/aws/elasticbeanstalk"
 )
 
 type AWS struct {
-	EC2                     ec2.EC2                        `tree:"ec2"`
-	CertificateManager      acm.CertificateManager         `tree:"acm"`
-	PCACertificateAuthority acmpca.PCACertificateAuthority `tree:"acmpca"`
-	APIGateway              apigateway.APIGateway          `tree:"apigateway"`
-	APIGatewayV2            apigatewayv2.APIGatewayV2      `tree:"apigatewayv2"`
-	AppAutoScaling          appautoscaling.AppAutoScaling  `tree:"appautoscaling"`
+	EC2                     ec2.EC2                           `tree:"ec2"`
+	CertificateManager      acm.CertificateManager            `tree:"acm"`
+	PCACertificateAuthority acmpca.PCACertificateAuthority    `tree:"acmpca"`
+	APIGateway              apigateway.APIGateway             `tree:"apigateway"`
+	APIGatewayV2            apigatewayv2.APIGatewayV2         `tree:"apigatewayv2"`
+	AppAutoScaling          appautoscaling.AppAutoScaling     `tree:"appautoscaling"`
+	ElasticBeanstalk        elasticbeanstalk.ElasticBeanstalk `tree:"elasticbeanstalk"`
 }
 
 func (aws *AWS) PostProcess() {

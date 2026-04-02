952e3bff501c25ecc057d4c7105157bea38181a2
Owen Rumney <owen@infracost.io>
2026-04-02 11:49:20 +0100

    feat: add directory service

--- pkg/tree/aws/aws.go
+++ pkg/tree/aws/aws.go
@@ -6,16 +6,18 @@ import (
 	"github.com/infracost/go-proto/pkg/tree/aws/apigateway"
 	"github.com/infracost/go-proto/pkg/tree/aws/apigatewayv2"
 	"github.com/infracost/go-proto/pkg/tree/aws/appautoscaling"
+	"github.com/infracost/go-proto/pkg/tree/aws/directoryservice"
 	"github.com/infracost/go-proto/pkg/tree/aws/ec2"
 )
 
 type AWS struct {
-	EC2                     ec2.EC2                        `tree:"ec2"`
-	CertificateManager      acm.CertificateManager         `tree:"acm"`
-	PCACertificateAuthority acmpca.PCACertificateAuthority `tree:"acmpca"`
-	APIGateway              apigateway.APIGateway          `tree:"apigateway"`
-	APIGatewayV2            apigatewayv2.APIGatewayV2      `tree:"apigatewayv2"`
-	AppAutoScaling          appautoscaling.AppAutoScaling  `tree:"appautoscaling"`
+	EC2                     ec2.EC2                           `tree:"ec2"`
+	CertificateManager      acm.CertificateManager            `tree:"acm"`
+	PCACertificateAuthority acmpca.PCACertificateAuthority    `tree:"acmpca"`
+	APIGateway              apigateway.APIGateway             `tree:"apigateway"`
+	APIGatewayV2            apigatewayv2.APIGatewayV2         `tree:"apigatewayv2"`
+	AppAutoScaling          appautoscaling.AppAutoScaling     `tree:"appautoscaling"`
+	DirectoryService        directoryservice.DirectoryService `tree:"directoryservice"`
 }
 
 func (aws *AWS) PostProcess() {

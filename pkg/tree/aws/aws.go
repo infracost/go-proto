369c7aae231f6dde95ce9648152e0402519d9526
Owen Rumney <owen@infracost.io>
2026-04-16 19:22:46 +0000

    feat: Add elasticsearch types (#47)
    Co-authored-by: Liam Galvin <liam@infracost.io>

--- pkg/tree/aws/aws.go
+++ pkg/tree/aws/aws.go
@@ -7,6 +7,7 @@ import (
 	"github.com/infracost/go-proto/pkg/tree/aws/apigatewayv2"
 	"github.com/infracost/go-proto/pkg/tree/aws/appautoscaling"
 	"github.com/infracost/go-proto/pkg/tree/aws/ec2"
+	"github.com/infracost/go-proto/pkg/tree/aws/elasticsearch"
 	"github.com/infracost/go-proto/pkg/tree/aws/fsx"
 )
 
@@ -17,6 +18,7 @@ type AWS struct {
 	APIGateway              apigateway.APIGateway          `tree:"apigateway"`
 	APIGatewayV2            apigatewayv2.APIGatewayV2      `tree:"apigatewayv2"`
 	AppAutoScaling          appautoscaling.AppAutoScaling  `tree:"appautoscaling"`
+	Elasticsearch           elasticsearch.Elasticsearch    `tree:"elasticsearch"`
 	FSx                     fsx.FSx                        `tree:"fsx"`
 }
 

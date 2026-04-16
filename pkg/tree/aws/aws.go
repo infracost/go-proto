1be21960ffd60dddb7d0c43c181293941f5c664c
Owen Rumney <owen@infracost.io>
2026-04-16 19:28:49 +0000

    feat: add backup types (#24)
    Co-authored-by: Liam Galvin <liam@infracost.io>

--- pkg/tree/aws/aws.go
+++ pkg/tree/aws/aws.go
@@ -6,6 +6,7 @@ import (
 	"github.com/infracost/go-proto/pkg/tree/aws/apigateway"
 	"github.com/infracost/go-proto/pkg/tree/aws/apigatewayv2"
 	"github.com/infracost/go-proto/pkg/tree/aws/appautoscaling"
+	"github.com/infracost/go-proto/pkg/tree/aws/backup"
 	"github.com/infracost/go-proto/pkg/tree/aws/ec2"
 	"github.com/infracost/go-proto/pkg/tree/aws/elasticsearch"
 	"github.com/infracost/go-proto/pkg/tree/aws/fsx"
@@ -18,6 +19,7 @@ type AWS struct {
 	APIGateway              apigateway.APIGateway          `tree:"apigateway"`
 	APIGatewayV2            apigatewayv2.APIGatewayV2      `tree:"apigatewayv2"`
 	AppAutoScaling          appautoscaling.AppAutoScaling  `tree:"appautoscaling"`
+	Backup                  backup.Backup                  `tree:"backup"`
 	Elasticsearch           elasticsearch.Elasticsearch    `tree:"elasticsearch"`
 	FSx                     fsx.FSx                        `tree:"fsx"`
 }

8507229391a1b27f9fc93424027c8d2bbca98528
Owen Rumney <owen@infracost.io>
2026-04-27 08:35:26 +0000

    feat: Add elasticache types (#45)
    Co-authored-by: Liam Galvin <liam@infracost.io>

---
+++ pkg/tree/aws/elasticache/elasticache.go
@@ -0,0 +1,47 @@
+package elasticache
+
+type ElastiCache struct {
+	Clusters          []Cluster          `tree:"clusters"`
+	ReplicationGroups []ReplicationGroup `tree:"replication_groups"`
+	ParameterGroups   []ParameterGroup   `tree:"parameter_groups"`
+}
+
+func (e *ElastiCache) PostProcess() {
+	for i, cluster := range e.Clusters {
+		// link cluster to parameter group
+		for j, pg := range e.ParameterGroups {
+			if pg.Name.Value() == cluster.ParameterGroupName.Value() || cluster.ParameterGroupName.Value() == pg.ID {
+				e.Clusters[i].Relationships.ParameterGroup = &e.ParameterGroups[j]
+				break
+			}
+		}
+
+		// link cluster to replication group
+		for j, rg := range e.ReplicationGroups {
+			if rg.ReplicationGroupID.Value() == cluster.ReplicationGroupID.Value() {
+				e.Clusters[i].Relationships.ReplicationGroup = &e.ReplicationGroups[j]
+
+				if cluster.Engine.IsDefaultOrEmpty() {
+					e.Clusters[i].Engine = rg.Engine
+				}
+				if cluster.EngineVersion.IsDefaultOrEmpty() {
+					e.Clusters[i].EngineVersion = rg.EngineVersion
+				}
+				if cluster.ParameterGroupName.IsDefaultOrEmpty() {
+					e.Clusters[i].ParameterGroupName = rg.ParameterGroupName
+				}
+
+				// link to parameter group from replication group
+				if e.Clusters[i].Relationships.ParameterGroup == nil {
+					for k, pg := range e.ParameterGroups {
+						if pg.Name.Value() == rg.ParameterGroupName.Value() || rg.ParameterGroupName.Value() == pg.ID {
+							e.Clusters[i].Relationships.ParameterGroup = &e.ParameterGroups[k]
+							break
+						}
+					}
+				}
+				break
+			}
+		}
+	}
+}

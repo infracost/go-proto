1cdaf74f2b342bc80dc7c1c07a304f9134c21349
Owen Rumney <owen@infracost.io>
2026-04-27 08:17:38 +0000

    feat: Add cloudtrail types (#30)
    Co-authored-by: Liam Galvin <liam@infracost.io>

--- pkg/tree/aws/aws.go
+++ pkg/tree/aws/aws.go
@@ -6,6 +6,7 @@ import (
 	"github.com/infracost/go-proto/pkg/tree/aws/apigateway"
 	"github.com/infracost/go-proto/pkg/tree/aws/apigatewayv2"
 	"github.com/infracost/go-proto/pkg/tree/aws/appautoscaling"
+	"github.com/infracost/go-proto/pkg/tree/aws/cloudtrail"
  	"github.com/infracost/go-proto/pkg/tree/aws/backup"
 	"github.com/infracost/go-proto/pkg/tree/aws/cloudfront"
 	"github.com/infracost/go-proto/pkg/tree/aws/cloudformation"
@@ -23,6 +24,7 @@ type AWS struct {
 	APIGateway              apigateway.APIGateway          `tree:"apigateway"`
 	APIGatewayV2            apigatewayv2.APIGatewayV2      `tree:"apigatewayv2"`
 	AppAutoScaling          appautoscaling.AppAutoScaling  `tree:"appautoscaling"`
+	Cloudtrail              cloudtrail.CloudTrail          `tree:"cloudtrail"`
 	Backup                  backup.Backup                  `tree:"backup"`
 	CloudFormation          cloudformation.CloudFormation  `tree:"cloudformation"`
   CloudFront              cloudfront.CloudFront          `tree:"cloudfront"`

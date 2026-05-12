d2cf3e50a8a7217d3aa6de06577d29b9fe5bf38c
Liam Galvin <liam@infracost.io>
2026-05-12 13:04:59 +0100

    fix: Fix AWS Batch typo (#60)

--- pkg/tree/aws/batch/batch.go
+++ pkg/tree/aws/batch/batch.go
@@ -1,6 +1,6 @@
 package batch
 
 type Batch struct {
-	ComputeEnvironemnts []ComputeEnvironment `tree:"compute_environment"`
+	ComputeEnvironments []ComputeEnvironment `tree:"compute_environment"`
 	JobDefinitions      []JobDefinition      `tree:"job_definition"`
 }

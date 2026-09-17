1d221f8565fcdbe5f83682e71abda9e43d5f2105
Liam Galvin <liam@infracost.io>
2026-09-17 15:37:06 +0100

    feat(sns): add archive retention days to SNS topic (#99)

--- pkg/tree/aws/sns/topic.go
+++ pkg/tree/aws/sns/topic.go
@@ -8,6 +8,8 @@ import (
 type Topic struct {
 	resource.Resource `tree:"-"`
 	FIFO              value.Bool `tree:"fifo"`
+	// ArchiveRetentionDays is 0 when message archiving is disabled.
+	ArchiveRetentionDays value.Int `tree:"archive_retention_days"`
 
 	Relationships TopicRelationships `tree:"-"`
 }
